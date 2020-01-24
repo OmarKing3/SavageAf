21981e018bf2295a671fc74cc34147768c20968c
OmarKing3 <60272453+OmarKing3@users.noreply.github.com>
2020-01-24 11:40:29 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("586876925566386177")
+setInterval(function() {
+channel.send(`OMAR KING OMAR KING OMAR KING OMAR KING`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
